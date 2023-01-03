614150bdbb2785ca8682dc6570cdcedb17015fe5
yossTheDev <yossthedev@gmail.com>
2023-01-03 10:49:40 -0500

    - Improve Donations Page

--- src/pages/Donations.tsx
+++ src/pages/Donations.tsx
@@ -1,5 +1,13 @@
-import { IconCreditCard, IconMoneybag, IconPhone } from '@tabler/icons';
+import {
+	IconCoffee,
+	IconCoinBitcoin,
+	IconCreditCard,
+	IconCurrencyDogecoin,
+	IconMoneybag,
+	IconPhone,
+} from '@tabler/icons';
 import React from 'react';
+import qvapay from '../assets/qvapay.svg';
 import { Navbar } from 'react-daisyui';
 import { MenuButton } from '../components/MenuButton';
 import { Clipboard } from '@capacitor/clipboard';
@@ -8,50 +16,86 @@ export const Donations: React.FC = () => {
 	return (
 		<>
 			{/* Nav Bar */}
-			<Navbar>
+			<Navbar className='dark:bg-base-200'>
 				<Navbar.Start>
 					<MenuButton></MenuButton>
 				</Navbar.Start>
-				<div className='flex-auto flex w-full '>
-					<p className='poppins-font-family  normal-cas font-bold mx-auto text-2xl text-center'>
-						Donations
-					</p>
-				</div>
+
+				<Navbar.Center>
+					<div className='flex-auto flex w-full mx-auto'>
+						<p className='poppins-font-family normal-cas font-bold  text-2xl text-center dark:text-white'>
+							Donations
+						</p>
+					</div>
+				</Navbar.Center>
+
+				<Navbar.End></Navbar.End>
 			</Navbar>
 
 			{/* Content*/}
-			<div className='overflow-scroll flex flex-auto flex-col select-none'>
-				<div className='mx-auto bg-gray-100 p-2 shadow rounded-full'>
-					<IconMoneybag className='text-yellow-500' size={52}></IconMoneybag>
+			<div className='overflow-scroll flex flex-auto flex-col select-none dark:bg-base-200'>
+				<div className='mx-auto bg-gray-100 dark:bg-gray-800 p-6 shadow rounded-full'>
+					<IconCoffee
+						className='mx-auto dark:text-white'
+						size={36}
+					></IconCoffee>
 				</div>
-				<p className='mx-auto text-2xl text-center font-bold m-2'>
+				<p className='mx-auto text-2xl text-center font-bold m-2 dark:text-white'>
 					Buy me a coffee ☕
 				</p>
-				<p className='m-2 text-gray-700'>
-					Show your love and buy me a coffee make a money transference to my
-					number or credit card
+				<p className='mx-auto text-center text-gray-700 mb-8'>
+					Click to copy to clipboard
 				</p>
 
-				<div className='mx-auto w-fit'>
-					<div className='shadow rounded-full inline-block m-2 p-2 bg-gray-50 hover:bg-gray-300'>
+				<div className='flex flex-wrap  items-center  mx-2'>
+					<div className='flex flex-auto shadow rounded-2xl  dark:bg-gray-800 m-2 p-4 bg-gray-200 hover:bg-gray-300'>
 						<IconCreditCard
-							className='text-green-600'
+							size={36}
+							className='text-green-600 mx-auto'
 							onClick={async () =>
 								await Clipboard.write({ string: '9238129970601219' })
 							}
 						></IconCreditCard>
 					</div>
 
-					<div className='shadow rounded-full inline-block m-2 p-2 bg-gray-50 hover:bg-gray-300'>
+					<div className='flex flex-auto  shadow rounded-2xl dark:bg-gray-800  m-2 p-4 bg-gray-200 hover:bg-gray-300'>
 						<IconPhone
-							className='text-blue-600'
+							size={36}
+							className='text-blue-600 mx-auto'
 							onClick={async () =>
 								await Clipboard.write({ string: '58389251' })
 							}
 						></IconPhone>
 					</div>
+
+					<div className='flex flex-auto  shadow rounded-2xl dark:bg-gray-800 m-2 p-4 bg-gray-200 hover:bg-gray-300'>
+						<IconCoinBitcoin
+							size={36}
+							className='text-yellow-400 mx-auto'
+							onClick={async () =>
+								await Clipboard.write({ string: '58389251' })
+							}
+						></IconCoinBitcoin>
+					</div>
+
+					<div className='flex flex-auto  shadow rounded-2xl dark:bg-gray-800 m-2 p-4 bg-gray-200 hover:bg-gray-300'>
+						<IconCurrencyDogecoin
+							size={36}
+							className='text-yellow-700 mx-auto'
+							onClick={async () =>
+								await Clipboard.write({ string: '58389251' })
+							}
+						></IconCurrencyDogecoin>
+					</div>
+
+					<a
+						href='https://qvapay.com/payme/yoannisgnw'
+						target={'_blank'}
+						className='flex flex-auto  shadow rounded-2xl dark:bg-gray-800 m-2 p-4 bg-gray-200 hover:bg-gray-300'
+					>
+						<img className='text-yellow-700 mx-auto h-8' src={qvapay}></img>
+					</a>
 				</div>
-				<p className='mx-auto text-center'>Click to copy to clipboard</p>
 			</div>
 		</>
 	);
